c083fa1f5bb8706c643a2f5d6dac4ff8003011a3
Space-wolfxp17 <sergeyxp17@gmail.com>
2026-04-02 17:55:59 +0300

    Update AppActions.kt

--- app/src/main/java/com/ordis/app/core/AppActions.kt
+++ app/src/main/java/com/ordis/app/core/AppActions.kt
@@ -2,4 +2,6 @@ package com.ordis.app.core
 
 object AppActions {
     var onToggleListening: (() -> Unit)? = null
+    var onExportMemory: (() -> Unit)? = null
+    var onClearMemory: (() -> Unit)? = null
 }
