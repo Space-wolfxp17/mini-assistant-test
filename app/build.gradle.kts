c643e69267eddfb05673379947ebebacb08ffffa
Space-wolfxp17 <sergeyxp17@gmail.com>
2026-04-02 18:32:14 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,10 +9,11 @@ android {
 
     defaultConfig {
         applicationId = "com.ordis.app"
-        minSdk = 28 // Android 9
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "1.0"
+    }
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
