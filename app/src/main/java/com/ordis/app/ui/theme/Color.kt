5c56fa20aa4828fdc65060292c37ac52c3916c16
Space-wolfxp17 <sergeyxp17@gmail.com>
2026-04-02 15:11:28 +0300

    Create Color.kt

---
+++ app/src/main/java/com/ordis/app/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.ordis.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val NeonCyan = Color(0xFF00F5FF)
+val NeonMagenta = Color(0xFFFF2BD6)
+val NeonPurple = Color(0xFF8A2BE2)
+val NeonGreen = Color(0xFF39FF14)
+
+val DarkBg = Color(0xFF0A0A12)
+val DarkBg2 = Color(0xFF111122)
+val CardBg = Color(0xFF17172A)
+
+val TextPrimary = Color(0xFFEAF7FF)
+val TextSecondary = Color(0xFF9AB0C7)
+val ErrorRed = Color(0xFFFF4D6D)
